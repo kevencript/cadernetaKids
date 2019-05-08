0a30aba79a61e1060407c8e329950610b392cf25
Gabriel Braga <kevencript@gmail.com>
2019-05-08 20:51:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi:'Hello Everyone!' });
+app.get('/larissa', (req, res) => {
+    res.send({ hi:'LarissaEmanuella fila da puta!' });
 })
 
 const PORT = process.env.PORT || 5000;
