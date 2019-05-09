74e3eceb695734ed5c412da1b83bdc454ea30033
Gabriel Braga <kevencript@gmail.com>
2019-05-08 22:29:43 -0300

    Installing PassportJs and GoogleStrategy

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send( 'Thats it bitch!!' );
-})
-
-app.get('/api', (req, res) => {
-
-    res.send('Im at the API!');
-});
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000;
 
