20e206bb6238662d5bd82ceb79f1d196d2dc72a8
Gabriel Braga <kevencript@gmail.com>
2019-05-08 21:53:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
     res.send( 'Thats it bitch!!' );
 })
 
+app.get('/api', (req, res) => {
+
+    res.send('Im at the API!');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
