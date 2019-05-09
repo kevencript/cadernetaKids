4a9daf6f8d6d74a054b6f912819075cfa463c119
Gabriel Braga <kevencript@gmail.com>
2019-05-08 21:43:47 -0300

    Added nodemon to Developing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send( 'Routes boy!' );
+    res.send( 'Thats it bitch!!' );
 })
 
 const PORT = process.env.PORT || 5000;
