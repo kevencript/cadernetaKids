af285b19878c830988e4a5a139317c7f55272d18
Gabriel Braga <kevencript@gmail.com>
2019-05-10 13:19:15 -0300

    Finished the google OAuth
    added cookieSession,
    initialized the passport
    defined an route to test the req.user

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./configs/keys');
 const requireDir = require('require-dir');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const loginRoutes = require('./routes/loginRoutes');
 
 requireDir('./models');
 require('./services/passport');
@@ -10,7 +13,19 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
+app.use(cookieSession({
+    maxAge: 24* 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 require('./routes/authRoutes')(app);
+app.use('/profile', loginRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -4,19 +4,41 @@ const keys           = require('../configs/keys');
 const mongoose       = require('mongoose');
 const User           = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
 passport.use(
     new GoogleStrategy({
         clientID: keys.GoogleClientID,
         clientSecret: keys.GoogleClientSecret,
         callbackURL: '/auth/google/callback'
     }, (acessToken, refreshToken, profile, done) => {
-            new User({ 
-                googleId: profile.id,
-                name: profile.displayName
-             }).save()
-             
-             .then(userCreated => {
-                 console.log(userCreated);
-             });
+            
+        User.findOne({ googleId: profile.id }).then(existingUser => {
+
+            if(existingUser){
+
+                console.log("User is: "+existingUser);
+                done(null, existingUser);
+            } else{
+
+                new User({ 
+                    googleId: profile.id,
+                    name: profile.displayName
+                }).save()
+                .then((userCreated) => {
+                    console.log("User created: "+userCreated);
+                    done(null, userCreated);
+                })
+            }
+
+        }); 
     }
 ));
