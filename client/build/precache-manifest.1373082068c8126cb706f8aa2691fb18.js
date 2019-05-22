23b65cc71e92445cf9dedae9e56588cb1eb428e4
Gabriel Braga <kevencript@gmail.com>
2019-05-21 23:35:16 -0300

    Woow

---
+++ client/build/precache-manifest.1373082068c8126cb706f8aa2691fb18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9834d826e78e09bdae187177b85c4a1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fe13d5479ff9ff8e761",
+    "url": "/static/css/main.df002511.chunk.css"
+  },
+  {
+    "revision": "294b1d87805404d664e6",
+    "url": "/static/js/2.433a665e.chunk.js"
+  },
+  {
+    "revision": "7fe13d5479ff9ff8e761",
+    "url": "/static/js/main.5301b88b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
